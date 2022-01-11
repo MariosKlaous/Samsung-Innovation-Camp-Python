22489b60debcc5d61f5ee76767e98604bc6d73b3
vangeliw <97253136+vangeliw@users.noreply.github.com>
2022-01-11 14:35:48 +0200

    Update Samsung Innovation Campus-Python Project.py

--- Samsung Innovation Campus-Python Project.py
+++ Samsung Innovation Campus-Python Project.py
@@ -1,3 +1,16 @@
 import pandas as pd
+#1.Διαβάστε το αρχείο data.csv
 data = pd.read_csv('C:/Users/mario/Documents/Personal/Project/Samsung Innovation Campus/Εργασία Python/data.csv', sep=',', header='infer')
-# imports library and reads the csv file as "data"
+#2.Εκτυπώστε τις τελευταίες 5 εγγραφές του dataframe
+print(data.iloc[541904:])
+#3.Πόσες (αριθμό) και ποιες (ονόματα) στήλες (columns) έχει το dataset που φορτώσατε;
+print(data.columns)
+len(data.columns)
+#4.Ως τι τύπο δεδομένων αναγνωρίζει η βιβλιοθήκη pandas τις στήλες του dataset;
+print(type(data.columns))
+columns= list(data.columns) #ΧΡΕΙΑΖΕΤΑΙ Η ΜΕΤΑΤΡΟΠΗ ΣΕ ΛΙΣΤΑ;;;
+print(type(columns))
+#5.Υπάρχουν στήλες με τιμές που λείπουν; Κι αν ναι ποιες; (Ποιες στήλες έχουν missing values /NaN)
+data.isnull().any()
+#6.Ποιος ο συνολικός αριθμών των εγγραφών; (χωρίς τα headers)
+len(data)
